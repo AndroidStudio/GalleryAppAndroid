625a126cb775d8f330b9ac73ac1eaeb3cde23314
Przemysław Skitał <przemek33665@gmail.com>
2019-03-31 19:59:01 +0200

    WebService code refactoring

--- MainModule/src/main/java/fdt/galleryapp/webservice/WebService.kt
+++ MainModule/src/main/java/fdt/galleryapp/webservice/WebService.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import fdt.galleryapp.R
 import fdt.galleryapp.utils.connectivityManager
 import io.reactivex.Single
-import io.reactivex.functions.BiFunction
 import okhttp3.ResponseBody
 import retrofit2.Response
 import javax.inject.Inject
@@ -12,11 +11,10 @@ import javax.inject.Inject
 open class WebService @Inject constructor(private val context: Context) {
 
     open fun request(request: Single<Response<ResponseBody>>): Single<String> {
-        return Single.zip(checkInternetConnection(), request.flatMap { mapResponse(it) },
-            BiFunction { _, response -> response })
+        return checkInternetConnection().flatMap { request.flatMap { mapResponse(it) } }
     }
 
-    open fun checkInternetConnection(): Single<Boolean> {
+    private fun checkInternetConnection(): Single<Boolean> {
         return Single.create {
             val activeNetwork = context.connectivityManager?.activeNetworkInfo
             if (activeNetwork != null && activeNetwork.isConnected) {
@@ -27,7 +25,7 @@ open class WebService @Inject constructor(private val context: Context) {
         }
     }
 
-    open fun mapResponse(responseBody: Response<ResponseBody>): Single<String> {
+    private fun mapResponse(responseBody: Response<ResponseBody>): Single<String> {
         return Single.create {
             val body = responseBody.body()
             if (responseBody.isSuccessful && body != null) {
